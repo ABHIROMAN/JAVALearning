f816a321e2615516914fbad422ebfd9d600f83b3
ABHIROMAN <abhishek.kumar2020@vitbhopal.ac.in>
2022-05-20 08:13:40 +0530

    pushing changes

---
+++ TRAINING_CLASS/CRT.java
@@ -0,0 +1,26 @@
+class CRT {
+
+    static int findMinX(int num[], int rem[], int k) {
+        int x = 1;
+        while (true) {
+            int j;
+            for (j=0; j<k; j++) {
+                if (x%num[j] != rem[j]) {
+                    break;
+            }
+        }
+            if (j == k)
+                return x;
+        x++;
+        }
+    }
+    public static void main(String[] args) {
+        int num[] = {3,4,5,7};
+        int rem[] = {1,1,1,0};
+
+        int k = num.length;
+
+        System.out.println("Value of X is" + findMinX(num, rem, k));
+
+    }
+}
